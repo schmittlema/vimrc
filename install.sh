1110c8358ab8b07d59ac963b43363ddb93e42efc
Matt Schmittle <schmttle@udel.edu>
2017-07-26 13:42:27 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update && apt-get install -y vim
-mv .vimrc $HOME/
+cp .vimrc $HOME/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
